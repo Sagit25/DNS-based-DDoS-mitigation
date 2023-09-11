a4afa40f816e68c9335732d2c3ff4bcf2b722782
GTPV <49084575+GTPV@users.noreply.github.com>
2023-09-11 14:43:37 +0900

    TCP SYN capture
    * packet_capture.c : captures tcp syn packets and print SourceIP and DestIP
    * Makefile : makefile for packet_capture.c

--- monitor/packet_capture.c
+++ monitor/packet_capture.c
@@ -13,25 +13,37 @@ void packet_handler(u_char *extra_user_data, const struct pcap_pkthdr *packet_he
 	// Only analyze TCP-SYN packets
 	if((tcp_header->th_flags & TH_SYN) && !(tcp_header->th_flags & TH_ACK)){
 		// TODO: check source ip address, check ISP, count number of packets
+		char source_ip[INET_ADDRSTRLEN];
+		char destination_ip[INET_ADDRSTRLEN];
+		inet_ntop(AF_INET, &(ip_header->ip_src), source_ip, INET_ADDRSTRLEN);
+		inet_ntop(AF_INET, &(ip_header->ip_dst), destination_ip, INET_ADDRSTRLEN);
+		printf("\nSource IP: %s,\tDest IP: %s\n", source_ip, destination_ip);
 	}
 }
 
 int main(int argc, char *argv[]){
+	int ret;
 	char *device;
 	char errbuf[PCAP_ERRBUF_SIZE];
+	pcap_if_t *alldevs;
+	pcap_if_t *d;
 	pcap_t *handle;
 
-	device = pcap_lookupdev(errbuf); // 'pcap_lookupdev' deprecated
-	// device = pcap_findalldevs(pcap_if_t **, char *)
-	if(device == NULL){
-		fprintf(stderr, "Couldn't find default device: %s\n", errbuf);
-		return(2);
+	ret = pcap_findalldevs(&alldevs, errbuf);
+	if(ret == -1){
+		fprintf(stderr, "Error in pcap_findalldevs: %s\n", errbuf);
+		return 2;
+	} else if(alldevs == NULL){
+		fprintf(stderr, "No device found.\n");
+		return 2;
 	}
 
+	d = alldevs;
+	device = d->name;
 	handle = pcap_open_live(device, BUFSIZ, 1, 1000, errbuf);
 	if(handle == NULL){
 		fprintf(stderr, "Couldn't open device %s: %s\n", device, errbuf);
-		return(2);
+		return 2;
 	}
 
 	pcap_loop(handle , 0, packet_handler, NULL);
