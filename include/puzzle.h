b13afb03c8f6458d1e3882720c77c30478892704
Sagit25 <sukhuny@snu.ac.kr>
2024-01-30 12:28:11 +0000

    ip

--- include/puzzle.h
+++ include/puzzle.h
@@ -27,3 +27,47 @@ struct chain_msg {
 int hash_ftn(int seed) {
     return seed+1;
 }
+
+unsigned int iptou(char* cp) {
+	unsigned int val;
+	int base;
+	char c;
+	unsigned int parts[4];
+	unsigned int *pp = parts;
+
+	for (;;) {
+		val = 0; base = 10;
+		if (*cp == '0') {
+			if (*++cp == 'x' || *cp == 'X')
+				base = 16, cp++;
+			else
+				base = 8;
+		}
+		while ((c = *cp) != '\0') {
+			if (isascii(c) && isdigit(c)) {
+				val = (val * base) + (c - '0');
+				cp++;
+				continue;
+			}
+			if (base == 16 && isascii(c) && isxdigit(c)) {
+				val = (val << 4) + 
+					(c + 10 - (islower(c) ? 'a' : 'A'));
+				cp++;
+				continue;
+			}
+			break;
+		}
+		if (*cp == '.') {
+			if (pp >= parts + 3 || val > 0xff)
+				return (0);
+			*pp++ = val, cp++;
+		} else
+			break;
+	}
+	if (*cp && (!isascii(*cp) || !isspace(*cp))) return (0);
+	if (val > 0xff) return (0);
+	val |= (parts[0] << 24) | (parts[1] << 16) | (parts[2] << 8);
+	return val;
+}
+
+
